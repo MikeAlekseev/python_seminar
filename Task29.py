a0c80d33b1522132a00cdc7ee3f3dbe1f4e58e15
MikeAlekseev <johnnynouz@yandex.ru>
2023-03-23 17:49:01 +0300

    Добавлены задачи 16 18 20

---
+++ Task29.py
@@ -0,0 +1,18 @@
+
+# Ваня:
+n = int(input())
+max_number = -1
+while n != 0:
+    n = int(input())
+    if max_number < n:
+        max_number = n
+print(max_number)
+
+# Петя:
+# n = int(input())
+# max_number = -1
+# while n < 0:
+#  n = int(input())
+#  if max_number < n:
+#  n = max_number
+# print(n)
